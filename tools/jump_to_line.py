12013fc8f308b250df0ed33edb3edef5e1e574ef
Jishnu kv <108184606+DesignLipsx@users.noreply.github.com>
2025-07-02 11:50:16 +0530

    Update jump_to_line.py

--- tools/jump_to_line.py
+++ tools/jump_to_line.py
@@ -2,26 +2,21 @@ import bpy
 
 
 def update_line_number(self, context):
-    text_editor = context.space_data.text
+    text = context.space_data.text
     props = context.window_manager.jump_to_line_props
-    if text_editor is not None:
-        lines = text_editor.as_string().split('\n')
 
+    if text:
+        max_line_number = len(text.lines)
         line_number = props.line_number
-        if line_number > 0:
-            # Calculate the maximum line number based on the
-            # actual number of lines in the script
-            max_line_number = len(lines)
-            if line_number > max_line_number:
-                line_number = max_line_number
-                props.line_number = line_number
 
-            bpy.ops.text.jump(line=line_number)
+        if line_number > max_line_number:
+            line_number = max_line_number
+            props.line_number = line_number
+
+        bpy.ops.text.jump(line=line_number)
 
 
 class JUMP_TO_LINE_PG_properties(bpy.types.PropertyGroup):
-    recent_list: bpy.props.CollectionProperty(type=bpy.types.PropertyGroup)
-    recent_list_index: bpy.props.IntProperty(name="Recent list index")
     line_number: bpy.props.IntProperty(
         name="Line Number",
         min=1,
@@ -32,10 +27,10 @@ class JUMP_TO_LINE_PG_properties(bpy.types.PropertyGroup):
 def register():
     bpy.utils.register_class(JUMP_TO_LINE_PG_properties)
     bpy.types.WindowManager.jump_to_line_props = bpy.props.PointerProperty(
-        type=JUMP_TO_LINE_PG_properties)
+        type=JUMP_TO_LINE_PG_properties
+    )
 
 
 def unregister():
-    bpy.utils.unregister_class(JUMP_TO_LINE_PG_properties)
     del bpy.types.WindowManager.jump_to_line_props
-
+    bpy.utils.unregister_class(JUMP_TO_LINE_PG_properties)
